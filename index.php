111b689e0a8ed211c61c58a650f1db7a90a8fba5
Grayson Crawford <crawfordg@jbu.edu>
2023-03-14 21:18:00 -0500

    tried adding autocomplete

--- index.php
+++ index.php
@@ -8,13 +8,19 @@
     <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Raleway'>
     <link rel="shortcut icon" type="image/x-icon" href="./images/favicon.ico" />
     <title>RateMyCaf | University Cafeteria Reviews</title>
-    <?php include("top-menu.php") ?>
+    <?php 
+        include("top-menu.php");
+        include("db_connection.php");
+    ?>
     <script src="js/RMCcode.js"></script>
 </head>
 <body>
     <div id="search-section"> 
         <p>Finding a place to eat just got a lot simpler.</p>
-        <input id="search-bar" placeholder="Search for your university" type="text">
+        <form name="search-bar-form" method="POST" action="autocomplete-process.php">
+            <input id="search-bar" placeholder="Search for your university" type="text" onkeyup="javascript:load_data(this.value)"/>
+            <span id="autosuggestions"></span>
+        </form>
     </div>
     <div id="information-section"> 
         <div>
