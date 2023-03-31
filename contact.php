8d56172d3dca3348d22cf21b50b56558161bdaa9
robPTY <rcadown8@gmail.com>
2023-03-30 20:15:06 -0500

    cant stop and we wont stop

--- contact.php
+++ contact.php
@@ -7,34 +7,6 @@
     include("./reusables/top-menu.php");
 ?>
 <?php
-    $sql2="SELECT * FROM `universities_tab`"; //Selects all books from books table
-    $result2=$conn->query($sql2);
-    $count=0;
-    echo "<table><tr>";
-    while($row2 = $result2->fetch_assoc())
-    {
-        echo "<td>";
-        echo "<div class='uni-cards'>";
-        echo "<div class='uni-card-img'>";
-        echo "<img class='uni-img' src='./images/".$row2['uni_img']."'/>";
-        echo "</div>";
-        echo "<div class='uni-cards-text'>";
-        echo "<div class='uni-card-name'>";
-        echo $row2['uni_short_name']."<br>";
-        echo "</div>";
-        echo $row2['uni_location'];
-        echo "</div>";
-        echo "</div>";
-        echo "</td>";
-        $count++;
-        if($count % 4 == 0){
-            echo "</tr><tr>";
-        }
-        if($count == 8){
-            break;
-        }
-    }
-    echo "</tr></table>";
     include("./reusables/footer.php");
 ?>
 
--- index.php
+++ index.php
@@ -66,29 +66,36 @@
         </div>
     </div>
     <div id="review-section">
-        <?php
-            $sql2="SELECT * FROM `universities_tab`"; //Selects all books from books table
-            $result2=$conn->query($sql2);
-			$count=0;
-			echo "<table><tr>";
-            while($row2 = $result2->fetch_assoc())
-            {
-				echo "<td>";
-                echo "<img class='uni-img' src='./images/".$row2['uni_img']."'/>";
-				echo "<br>";
-                echo $row2['uni_name']."<br>";
-                echo $row2['uni_location']."<br>";
-				echo "</td>";
-				$count++;
-				if($count % 4 == 0){
-					echo "</tr><tr>";
-				}
-                if($count == 8){
-                    break;
-                }
+    <?php
+        $sql2="SELECT * FROM `universities_tab`"; //Selects all books from books table
+        $result2=$conn->query($sql2);
+        $count=0;
+        echo "<table><tr>";
+        while($row2 = $result2->fetch_assoc())
+        {
+            echo "<td>";
+            echo "<div class='uni-cards'>";
+            echo "<div class='uni-card-img'>";
+            echo "<img class='uni-img' src='./images/".$row2['uni_img']."'/>";
+            echo "</div>";
+            echo "<div class='uni-cards-text'>";
+            echo "<div class='uni-card-name'>";
+            echo "<span class='boldt'>".$row2['uni_short_name']."</span><br>";
+            echo "</div>";
+            echo $row2['uni_location'];
+            echo "</div>";
+            echo "</div>";
+            echo "</td>";
+            $count++;
+            if($count % 4 == 0){
+                echo "</tr><tr>";
             }
-			echo "</tr></table>";
-        ?>
+            if($count == 8){
+                break;
+            }
+        }
+        echo "</tr></table>";
+    ?>
     </div>
     <?php
         include("./reusables/footer.php");
