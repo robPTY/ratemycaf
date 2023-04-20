b8c69c8ac1b2788b04792190e19a4e630cd4ba72
Grayson Crawford <crawfordg@jbu.edu>
2023-04-20 10:11:00 -0500

    got login working with backend

--- js/RMCcode.js
+++ js/RMCcode.js
@@ -13,3 +13,11 @@ $(function (){
         }
     });
 });
+
+//Refresh Captcha
+function refreshCaptcha(){
+    var img = document.images['captcha_image'];
+    img.src = img.src.substring(
+		0,img.src.lastIndexOf("?")
+		)+"?rand="+Math.random()*1000;
+}
--- login.php
+++ login.php
@@ -1,15 +1,34 @@
+<?php session_start() ?>
+
 <!DOCTYPE html>
 <html>
     <head>
         <link href="./css/LOGstyles.css" rel="stylesheet"/>
         <link rel="shortcut icon" type="image/x-icon" href="./images/favicon.ico" />
     </head>
+    <script src="js/RMCcode.js"></script>
+
     <body>
         <div id="login-section">
-            <form>
+            <form method="POST" action="login_redirect.php">
                 <h1 class='login-title'> Log In </h1>
-                <input class="name" type="email" placeholder="E-Mail/Username"/>
-                <input class="name" type="Password" placeholder="Password"/>
+                <input class="name" id="email" name="email" type="email" placeholder="E-Mail"/>
+                <input class="name" id="password" name="password" type="Password" placeholder="Password"/>
+                <label><strong>Enter Captcha:</strong></label><br />
+                <input type="text" name="captcha" />
+                <p><br />
+                <img src="captcha.php?rand=<?php echo rand(); ?>" id='captcha_image'>
+                </p>
+                <p>Can't read the image?
+                <a href='javascript: refreshCaptcha();'>click here</a>
+                to refresh</p>
+                <?php
+                  if (isset($_SESSION['message']))
+                  {
+                      echo $_SESSION['message'];
+                      unset($_SESSION['message']);
+                  }
+                ?>
                 <input class="submit" type="submit" id="login-button" value="Log In"/>
                 <a href="#"><p>Forgot password?</p></a>
                 <a href="register.php"><p>Don't have an account? Sign Up</p></a>
